d35d0f73bc92cf9779e8f8aa694f8c70a3211a3f
ninaschramm <schrammnina@outlook.com>
2022-11-17 20:21:28 -0300

    initial tickets router config

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { loadEnv, connectDb, disconnectDB } from "@/config";
 loadEnv();
 
 import { handleApplicationErrors } from "@/middlewares";
-import { usersRouter, authenticationRouter, eventsRouter, enrollmentsRouter, paymentsRouter } from "@/routers";
+import { usersRouter, authenticationRouter, eventsRouter, enrollmentsRouter, paymentsRouter, ticketsRouter } from "@/routers";
 
 const app = express();
 app
@@ -20,6 +20,7 @@ app
   .use("/event", eventsRouter)
   .use("/enrollments", enrollmentsRouter)
   .use("/payments", paymentsRouter)
+  .use("/tickets", ticketsRouter)
   .use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
---
+++ src/routers/tickets-router.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { } from "@/controllers";
+
+const ticketsRouter = Router();
+
+ticketsRouter.get("/", );
+
+export { ticketsRouter };
